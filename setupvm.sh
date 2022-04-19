615558bec192001fdcaa171dbddd22184ef6449e
simachenko <andrewsimachenko@outlook.com>
2022-04-19 10:23:08 +0300

    Remove sudo

--- setupvm.sh
+++ setupvm.sh
@@ -9,8 +9,8 @@ apt-get install -y kubelet=1.15.4-00 kubeadm=1.15.4-00 kubectl=1.15.4-00 docker.
 if [$1 = $'master']
     kubeadm init
     mkdir -p $HOME/.kube
-    sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-    sudo chown $(id -u):$(id -g) $HOME/.kube/config
+    cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    chown $(id -u):$(id -g) $HOME/.kube/config
 fi
 
 kubeadm join --token 702ff6.bc7aacff7aacab17 174.138.15.158:6443 --discovery-token-ca-cert-hash sha256:68bc22d2c631800fd358a6d7e3998e598deb2980ee613b3c2f1da8978960c8ab
